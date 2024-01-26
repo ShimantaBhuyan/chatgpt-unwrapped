37f6badcf4ff3cf22a01b8eb8a16ed68f7ae5906
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2024-01-27 00:18:39 +0530

    sunset until next year

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 import { initDB } from "react-indexed-db-hook";
 import { DBConfig } from "@/lib/dbconfig";
 import { useIndexedDB } from "react-indexed-db-hook";
-import OnboardingPage from "@/pages/OnboardingPage";
-import { LOCAL_STORAGE_KEY } from "@/lib/constants";
+// import OnboardingPage from "@/pages/OnboardingPage";
+// import { LOCAL_STORAGE_KEY } from "@/lib/constants";
 import useLocalStorage from "@/lib/use-local-storage";
 import Wrapped from "@/pages/Wrapped";
-import Loading from "@/components/Loading";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import useInterval from "@/lib/poll";
+// import Loading from "@/components/Loading";
+// import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+// import useInterval from "@/lib/poll";
 import { cn } from "./lib/utils";
 import AnimatedLogo from "@/assets/animated_logo.svg?react";
 import ShowcaseImage from "@/assets/showcase.png";
@@ -18,12 +18,12 @@ import ShowcaseImage from "@/assets/showcase.png";
 initDB(DBConfig);
 
 function App() {
-  const { add, getByID } = useIndexedDB("chatgpt-unwrapped");
+  const { /* add, */ getByID } = useIndexedDB("chatgpt-unwrapped");
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [_, setKey] = useLocalStorage<string>(LOCAL_STORAGE_KEY, "", true);
-  const [jobID, setJobID] = useLocalStorage<string>("JOB_ID", "", true);
-  const [isJobRunning, setIsJobRunning] = useState(false);
-  const [isAnalysing, setIsAnalysing] = useState(false);
+  // const [_, setKey] = useLocalStorage<string>(LOCAL_STORAGE_KEY, "", true);
+  // const [jobID, setJobID] = useLocalStorage<string>("JOB_ID", "", true);
+  // const [isJobRunning, setIsJobRunning] = useState(false);
+  // const [isAnalysing, setIsAnalysing] = useState(false);
 
   const [isUnwrappedAlready, setIsUnwrappedAlready] = useState(false);
 
@@ -56,105 +56,105 @@ function App() {
         data?.conversations.length > 0
       ) {
         setIsUnwrappedAlready(true);
-        setIsJobRunning(false);
+        // setIsJobRunning(false);
       } else {
         setIsUnwrappedAlready(false);
       }
     });
   }, [getByID]);
 
-  const onGenerate = async (token: string) => {
-    if (!token) {
-      alert("Please enter your OpenAI session key");
-      setIsAnalysing(false);
-    }
+  // const onGenerate = async (token: string) => {
+  //   if (!token) {
+  //     alert("Please enter your OpenAI session key");
+  //     setIsAnalysing(false);
+  //   }
 
-    setKey(token);
-    setIsAnalysing(true);
-    const response = await fetch(
-      `${import.meta.env.VITE_BASE_API_ENDPOINT}/get-wrapped`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          token: token,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        mode: "cors",
-      }
-    );
-    if (response.status === 401) {
-      alert(
-        "Invalid OpenAI session key. Please follow instructions and try again."
-      );
-      setIsAnalysing(false);
-      return;
-    } else if (response.status === 500) {
-      alert("Something went wrong. Please try again.");
-      setIsAnalysing(false);
-      return;
-    }
-    const data = await response.json();
-    setJobID(data.uuid);
-  };
+  //   setKey(token);
+  //   setIsAnalysing(true);
+  //   const response = await fetch(
+  //     `${import.meta.env.VITE_BASE_API_ENDPOINT}/get-wrapped`,
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         token: token,
+  //       }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       mode: "cors",
+  //     }
+  //   );
+  //   if (response.status === 401) {
+  //     alert(
+  //       "Invalid OpenAI session key. Please follow instructions and try again."
+  //     );
+  //     setIsAnalysing(false);
+  //     return;
+  //   } else if (response.status === 500) {
+  //     alert("Something went wrong. Please try again.");
+  //     setIsAnalysing(false);
+  //     return;
+  //   }
+  //   const data = await response.json();
+  //   setJobID(data.uuid);
+  // };
 
-  const POLL_INTERVAL = 15000;
+  // const POLL_INTERVAL = 15000;
 
-  const pollProgress = async () => {
-    if (jobID == undefined || jobID.length == 0 || isUnwrappedAlready) {
-      clearPoll();
-      return;
-    }
-    const response = await fetch(
-      `${import.meta.env.VITE_BASE_API_ENDPOINT}/progress/${jobID}`,
-      {
-        method: "GET",
-        // headers: {
-        //   "Content-Type": "application/json",
-        // },
-        mode: "cors",
-      }
-    );
+  // const pollProgress = async () => {
+  //   if (jobID == undefined || jobID.length == 0 || isUnwrappedAlready) {
+  //     clearPoll();
+  //     return;
+  //   }
+  //   const response = await fetch(
+  //     `${import.meta.env.VITE_BASE_API_ENDPOINT}/progress/${jobID}`,
+  //     {
+  //       method: "GET",
+  //       // headers: {
+  //       //   "Content-Type": "application/json",
+  //       // },
+  //       mode: "cors",
+  //     }
+  //   );
 
-    // if(response.status == 500) {
-    //   alert("Something went wrong. Please try again.");
-    //   setIsAnalysing(false);
-    //   return;
-    // }
+  //   // if(response.status == 500) {
+  //   //   alert("Something went wrong. Please try again.");
+  //   //   setIsAnalysing(false);
+  //   //   return;
+  //   // }
 
-    const data = await response.json();
-    if (data.status != "completed") {
-      if (data.status === "in_progress") {
-        setIsJobRunning(true);
-      }
-      return;
-    } else {
-      add({
-        conversations: data.data.conversations,
-        categorised: data.data.categorisedMap,
-        userTitle: data.data.userTitle,
-      }).then(
-        () => {
-          setIsUnwrappedAlready(true);
-          setIsJobRunning(false);
-          setIsAnalysing(false);
-        },
-        () => {
-          // console.log("ERROR: ", error);
-          alert("Something went wrong. Please try again.");
-        }
-      );
-    }
-  };
+  //   const data = await response.json();
+  //   if (data.status != "completed") {
+  //     if (data.status === "in_progress") {
+  //       setIsJobRunning(true);
+  //     }
+  //     return;
+  //   } else {
+  //     add({
+  //       conversations: data.data.conversations,
+  //       categorised: data.data.categorisedMap,
+  //       userTitle: data.data.userTitle,
+  //     }).then(
+  //       () => {
+  //         setIsUnwrappedAlready(true);
+  //         setIsJobRunning(false);
+  //         setIsAnalysing(false);
+  //       },
+  //       () => {
+  //         // console.log("ERROR: ", error);
+  //         alert("Something went wrong. Please try again.");
+  //       }
+  //     );
+  //   }
+  // };
 
-  const clearPoll = useInterval(pollProgress, POLL_INTERVAL);
+  // const clearPoll = useInterval(pollProgress, POLL_INTERVAL);
 
-  useEffect(() => {
-    if (isUnwrappedAlready) {
-      clearPoll();
-    }
-  }, [isUnwrappedAlready, clearPoll, getByID]);
+  // useEffect(() => {
+  //   if (isUnwrappedAlready) {
+  //     clearPoll();
+  //   }
+  // }, [isUnwrappedAlready, clearPoll, getByID]);
 
   if (isMobile) {
     return (
@@ -164,6 +164,9 @@ function App() {
           <h1 className="text-5xl md:text-7xl font-medium bricolage text-transparent bg-clip-text bg-gradient-to-tl from-green-700 via-teal-900 to-indigo-600 text-center">
             ChatGPT UnWrapped 2023
           </h1>
+          <h2 className="text-3xl md:text-5xl text-center">
+            Until next year!!
+          </h2>
           <h2 className="font-semibold text-center">
             80,000+ conversations unwrapped!
           </h2>
@@ -235,10 +238,10 @@ function App() {
     <div
       className={cn(
         "flex min-h-screen relative flex-col justify-center items-center p-8 lg:p-24 lg:pt-16 gap-20 pb-24",
-        !isUnwrappedAlready && !isAnalysing && "fullPageBackground"
+        !isUnwrappedAlready /* && !isAnalysing */ && "fullPageBackground"
       )}
     >
-      {isJobRunning || isAnalysing ? (
+      {/* {isJobRunning || isAnalysing ? (
         <>
           <div className="flex flex-col gap-5 items-center justify-center">
             <AnimatedLogo className="w-20 h-20" />
@@ -263,24 +266,29 @@ function App() {
             </p>
           </div>
         </>
-      ) : (
-        <>
-          <div className="flex flex-col gap-5 items-center justify-center">
-            <AnimatedLogo className="w-20 h-20" />
-            <h1 className="text-5xl md:text-7xl font-medium bricolage text-transparent bg-clip-text bg-gradient-to-tl from-green-700 via-teal-900 to-indigo-600 text-center">
-              ChatGPT UnWrapped 2023
-            </h1>
-            {!isUnwrappedAlready && (
-              <h2 className="text-lg font-semibold">
-                80,000+ conversations unwrapped!
-              </h2>
-            )}
-          </div>
-          {!isMobile ? (
-            <div className="flex h-full flex-col items-center w-full">
-              {!isUnwrappedAlready ? (
-                <>
-                  <Dialog>
+      ) : ( */}
+      <>
+        <div className="flex flex-col gap-5 items-center justify-center">
+          <AnimatedLogo className="w-20 h-20" />
+          <h1 className="text-5xl md:text-7xl font-medium bricolage text-transparent bg-clip-text bg-gradient-to-tl from-green-700 via-teal-900 to-indigo-600 text-center">
+            ChatGPT UnWrapped 2023
+          </h1>
+          {!isUnwrappedAlready && (
+            <h2 className="text-3xl md:text-5xl text-center">
+              Until next year!!
+            </h2>
+          )}
+          {!isUnwrappedAlready && (
+            <h2 className="text-lg font-semibold">
+              80,000+ conversations unwrapped!
+            </h2>
+          )}
+        </div>
+        {!isMobile ? (
+          <div className="flex h-full flex-col items-center w-full">
+            {!isUnwrappedAlready ? (
+              <>
+                {/* <Dialog>
                     <DialogTrigger asChild>
                       <button className="relative w-[200px] inline-flex items-center justify-center px-6 py-8 overflow-hidden font-medium text-center text-black transition duration-300 ease-out border-2 border-b-4 border-black rounded-full shadow-md group">
                         {" "}
@@ -326,42 +334,42 @@ function App() {
                     <DialogContent className="w-3/4 max-w-full p-10 h-full overflow-y-auto justify-center">
                       <OnboardingPage onSubmit={onGenerate} />
                     </DialogContent>
-                  </Dialog>
+                  </Dialog> */}
 
-                  <div className="flex flex-col gap-2 items-center justify-center mt-20">
-                    <p className="text-xs">As seen on</p>
-                    <div className="flex items-center space-x-2">
-                      <div className="h-7 w-7 overflow-hidden rounded-wt">
-                        <figure className="flex-shrink-0 aspect-square relative h-full overflow-hidden w-full">
-                          <img
-                            className="absolute inset-0 h-full w-full object-cover"
-                            width="100"
-                            height="100"
-                            src="https://news.bensbites.co/images/logo.jpg"
-                            alt="Ben's Bites logo"
-                          />
-                        </figure>
-                      </div>
-                      <a
-                        className="uppercase sm:block text-md font-bold underline"
-                        href="https://bensbites.beehiiv.com/p/daily-digest-reality-ai-jobs"
-                      >
-                        Ben's Bites
-                      </a>
+                <div className="flex flex-col gap-2 items-center justify-center mt-20">
+                  <p className="text-xs">As seen on</p>
+                  <div className="flex items-center space-x-2">
+                    <div className="h-7 w-7 overflow-hidden rounded-wt">
+                      <figure className="flex-shrink-0 aspect-square relative h-full overflow-hidden w-full">
+                        <img
+                          className="absolute inset-0 h-full w-full object-cover"
+                          width="100"
+                          height="100"
+                          src="https://news.bensbites.co/images/logo.jpg"
+                          alt="Ben's Bites logo"
+                        />
+                      </figure>
                     </div>
+                    <a
+                      className="uppercase sm:block text-md font-bold underline"
+                      href="https://bensbites.beehiiv.com/p/daily-digest-reality-ai-jobs"
+                    >
+                      Ben's Bites
+                    </a>
                   </div>
-                </>
-              ) : (
-                <Wrapped />
-              )}
-            </div>
-          ) : (
-            <h2 className="text-center">
-              Please open in desktop to generate your ChatGPT UnWrapped
-            </h2>
-          )}
-        </>
-      )}
+                </div>
+              </>
+            ) : (
+              <Wrapped />
+            )}
+          </div>
+        ) : (
+          <h2 className="text-center">
+            Please open in desktop to generate your ChatGPT UnWrapped
+          </h2>
+        )}
+      </>
+      {/* )} */}
 
       {/* {isJobRunning || isAnalysing ? ( */}
       <footer className="fixed bottom-0 left-0 z-20 w-full bg-white flex flex-col border-t border-gray-200 shadow-2xl gap-2 items-center justify-center p-4">
